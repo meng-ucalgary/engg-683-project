979b08b975fa18c242f1e4c51ba04e3357b3fcd7
Bhavyai Gupta <zbhavyai@gmail.com>
2022-03-30 00:50:33 -0600

    updating index.js

--- Prototype/src/index.js
+++ Prototype/src/index.js
@@ -1,59 +1,36 @@
-import React from "react";
-import reactDom from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import { BrowserRouter } from "react-router-dom";
-
-import 'font-awesome/css/font-awesome.css';
-import './index.css';
-
-
-
-import Login from "./components/login";
-
-import {Route, Switch} from 'react-router-dom';
-
-
-
-import RegistrationPage from "./components/registerpage";
-
-import YourAccount from "./components/youraccount";
-
-import MainMenu from "./components/mainmenu";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import RegistrationPage from './pages/Register';
+import YourAccount from './components/youraccount';
+import MainMenu from './components/mainmenu';
 import ErrorPage from './components/errorpage';
-import HomePage from './components/homepage';
-
 import ProductLine from './components/productline';
 import TopUp from './components/topup';
 import ReportManagement from './components/reportmanagement';
-import BasicReport from "./components/basicreport";
+import BasicReport from './components/basicreport';
 import GoldReport from './components/goldreport';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/products' element={<ProductLine />} />
+        <Route path='/goldreportsample' element={<GoldReport />} />
+        <Route path='/basicreportsample' element={<BasicReport />} />
+        <Route path='/register' element={<RegistrationPage />} />
+        <Route path='/youraccount' element={<YourAccount />} />
+        <Route path='/menu' element={<MainMenu />} />
+        <Route path='/report' element={<ReportManagement />} />
+        <Route path='/topup' element={<TopUp />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 
-
-reactDom.render(
-
-
-<BrowserRouter>
-
-
-    <Switch>
-
-      <Route path="/login" component={Login}/>
-      <Route path="/products" component={ProductLine}/>
-      <Route path="/goldreportsample" component={GoldReport}/>
-      <Route path="/basicreportsample" component={BasicReport}/>
-      <Route path='/register' component={RegistrationPage}/>
-      <Route path='/youraccount' component={YourAccount}/>
-      <Route path="/menu" component={MainMenu} />
-      <Route path="/report" component={ReportManagement} />
-      <Route path="/topup" component={TopUp} />
-      <Route path="/" component={HomePage} />
-      <Route path='*' component={ErrorPage}/>
-      </Switch>
-
-
-
-</BrowserRouter>,document.getElementById('root'));
-
+  document.getElementById('root')
+);
