059887fc07427d382ce8689b52ad223fc667a85b
Bhavyai Gupta <zbhavyai@gmail.com>
2022-03-31 00:23:52 -0600

    removing delays in loading, updating sign up link

--- Prototype/src/pages/HomePage.js
+++ Prototype/src/pages/HomePage.js
@@ -11,11 +11,11 @@ const Home = () => {
       <section id='hero' className='d-flex align-items-center'>
         <div className='container'>
           <div className='row'>
-            <div className='col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1' data-aos='fade-up' data-aos-delay='200'>
+            <div className='col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1'>
               <h1>Ultimate solution to reform your online reputation</h1>
               <h2>Don't let Social Media blowback your future</h2>
             </div>
-            <div className='col-lg-6 order-1 order-lg-2 hero-img' data-aos='zoom-in' data-aos-delay='200'>
+            <div className='col-lg-6 order-1 order-lg-2 hero-img' data-aos='zoom-in'>
               <img src={`${process.env.PUBLIC_URL + 'assets/img/hero-img.png'}`} className='img-fluid animated' alt='' />
             </div>
           </div>
@@ -78,12 +78,7 @@ const Home = () => {
                 </div>
               </div>
 
-              <div
-                className='col-lg-5 align-items-stretch order-2 order-lg-1 img'
-                style={{ backgroundImage: `url(${process.env.PUBLIC_URL + 'assets/img/why-us.png'})` }}
-                data-aos='zoom-in'
-                data-aos-delay='150'
-              >
+              <div className='col-lg-5 align-items-stretch order-2 order-lg-1 img' style={{ backgroundImage: `url(${process.env.PUBLIC_URL + 'assets/img/why-us.png'})` }} data-aos='zoom-in'>
                 &nbsp;
               </div>
             </div>
@@ -93,7 +88,7 @@ const Home = () => {
         <section id='skills' className='skills'>
           <div className='container' data-aos='fade-up'>
             <div className='row'>
-              <div className='col-lg-6 pt-4 pt-lg-0 content' data-aos='fade-left' data-aos-delay='100'>
+              <div className='col-lg-6 pt-4 pt-lg-0 content' data-aos='fade-left'>
                 <h3>Problematic posts can be on any social media!</h3>
                 <p className='fst-italic'>As per research XXX, very high percentage of posts on each social media platform can be problematic</p>
 
@@ -144,7 +139,7 @@ const Home = () => {
                   </div>
                 </div>
               </div>
-              <div className='col-lg-6 d-flex align-items-center' data-aos='fade-right' data-aos-delay='100'>
+              <div className='col-lg-6 d-flex align-items-center' data-aos='fade-right'>
                 <img src={`${process.env.PUBLIC_URL + 'assets/img/skills.png'}`} className='img-fluid' alt='' />
               </div>
             </div>
@@ -159,7 +154,7 @@ const Home = () => {
             </div>
 
             <div className='row'>
-              <div className='col-xl-4 col-md-6 d-flex align-items-stretch' data-aos='zoom-in' data-aos-delay='100'>
+              <div className='col-xl-4 col-md-6 d-flex align-items-stretch' data-aos='zoom-in'>
                 <div className='icon-box'>
                   <div className='icon'>
                     <i className='bx bx-layer'></i>
@@ -171,7 +166,7 @@ const Home = () => {
                 </div>
               </div>
 
-              <div className='col-xl-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0' data-aos='zoom-in' data-aos-delay='200'>
+              <div className='col-xl-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0' data-aos='zoom-in'>
                 <div className='icon-box'>
                   <div className='icon'>
                     <i className='bx bx-file'></i>
@@ -183,7 +178,7 @@ const Home = () => {
                 </div>
               </div>
 
-              <div className='col-xl-4 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0' data-aos='zoom-in' data-aos-delay='300'>
+              <div className='col-xl-4 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0' data-aos='zoom-in'>
                 <div className='icon-box'>
                   <div className='icon'>
                     <i className='bx bx-tachometer'></i>
@@ -206,7 +201,7 @@ const Home = () => {
                 <p> Don't wait. Get started today.</p>
               </div>
               <div className='col-lg-3 cta-btn-container text-center'>
-                <Link className='cta-btn align-middle' to='/register'>
+                <Link className='cta-btn align-middle' to='/pricing'>
                   Sign up
                 </Link>
               </div>
@@ -215,7 +210,7 @@ const Home = () => {
         </section>
 
         <section id='team' className='team section-bg'>
-          <div className='container' data-aos='fade-up'>
+          <div className='container'>
             <div className='section-title'>
               <h2>Team</h2>
               <p>Our highly talented team of experts will help you with your social media reputation</p>
@@ -223,7 +218,7 @@ const Home = () => {
 
             <div className='row'>
               <div className='col-lg-6'>
-                <div className='member d-flex align-items-start' data-aos='zoom-in' data-aos-delay='100'>
+                <div className='member d-flex align-items-start'>
                   <div className='pic text-center'>
                     <img src={`${process.env.PUBLIC_URL + '/team/lego_superman.jpg'}`} className='img-fluid' alt='' style={{ height: '150px' }} />
                   </div>
@@ -250,7 +245,7 @@ const Home = () => {
               </div>
 
               <div className='col-lg-6 mt-4 mt-lg-0'>
-                <div className='member d-flex align-items-start' data-aos='zoom-in' data-aos-delay='200'>
+                <div className='member d-flex align-items-start'>
                   <div className='pic text-center'>
                     <img src={`${process.env.PUBLIC_URL + '/team/lego_green_lantern.png'}`} className='img-fluid' alt='' style={{ height: '150px' }} />
                   </div>
@@ -277,7 +272,7 @@ const Home = () => {
               </div>
 
               <div className='col-lg-6 mt-4'>
-                <div className='member d-flex align-items-start' data-aos='zoom-in' data-aos-delay='400'>
+                <div className='member d-flex align-items-start'>
                   <div className='pic text-center'>
                     <img src={`${process.env.PUBLIC_URL + '/team/lego_batman.jpg'}`} className='img-fluid' alt='' style={{ height: '150px' }} />
                   </div>
@@ -304,7 +299,7 @@ const Home = () => {
               </div>
 
               <div className='col-lg-6 mt-4'>
-                <div className='member d-flex align-items-start' data-aos='zoom-in' data-aos-delay='300'>
+                <div className='member d-flex align-items-start'>
                   <div className='pic text-center'>
                     <img src={`${process.env.PUBLIC_URL + '/team/lego_aquaman.jpg'}`} className='img-fluid' alt='' style={{ height: '150px' }} />
                   </div>
@@ -331,7 +326,7 @@ const Home = () => {
               </div>
 
               <div className='col-lg-6 mt-4'>
-                <div className='member d-flex align-items-start' data-aos='zoom-in' data-aos-delay='400'>
+                <div className='member d-flex align-items-start'>
                   <div className='pic text-center'>
                     <img src={`${process.env.PUBLIC_URL + '/team/lego_cyborg.png'}`} className='img-fluid' alt='' style={{ height: '150px' }} />
                   </div>
