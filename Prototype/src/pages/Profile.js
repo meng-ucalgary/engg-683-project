c3e855caba49d0cea7eabd01c844037655ec684b
Bhavyai Gupta <zbhavyai@gmail.com>
2022-03-31 01:28:39 -0600

    removing unused imports

--- Prototype/src/pages/Profile.js
+++ Prototype/src/pages/Profile.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../helpers/useAuth';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
